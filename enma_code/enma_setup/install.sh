3485cc312b021691a15d6e8f4342618932b1f895
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2021-05-20 16:11:36 +0200

    corrected sudo issue

--- enma_code/enma_setup/install.sh
+++ enma_code/enma_setup/install.sh
@@ -1,35 +1,35 @@
 #! /bin/bash
-sudo apt update
-sudo apt install -y python
-sudo apt install -y python-dev
-sudo apt install -y openssh-client
-sudo apt install -y curl
-sudo apt install -y unzip
-sudo apt install -y tar
-sudo apt install -y wget
-sudo apt install -y build-essential
-sudo apt install -y manpages-dev
-sudo apt install -y python-pip
-sudo apt install -y python3-pip
-sudo apt install -y libsasl2-dev
-sudo apt install -y python-tk
-sudo apt install -y virtualenv
-sudo apt install -y libssl-dev
-sudo apt install -y libffi-dev
-sudo apt install -y libxml2-dev
-sudo apt install -y libxslt1-dev
-sudo apt install -y zlib1g-dev
-sudo apt install -y ifupdown2
-sudo apt install -y docker.io
-sudo apt install -y chrony
+apt update
+apt install -y python
+apt install -y python-dev
+apt install -y openssh-client
+apt install -y curl
+apt install -y unzip
+apt install -y tar
+apt install -y wget
+apt install -y build-essential
+apt install -y manpages-dev
+apt install -y python-pip
+apt install -y python3-pip
+apt install -y libsasl2-dev
+apt install -y python-tk
+apt install -y virtualenv
+apt install -y libssl-dev
+apt install -y libffi-dev
+apt install -y libxml2-dev
+apt install -y libxslt1-dev
+apt install -y zlib1g-dev
+apt install -y ifupdown2
+apt install -y docker.io
+apt install -y chrony
 
-sudo ulimit -n 10000
+ulimit -n 10000
 printf "*\thard\tnofile\t10000\n*\tsoft\tnofile\t10000" >> /etc/security/limits.conf
-sudo systemctl enable chronyd
+systemctl enable chronyd
 # change for other linux versions
-sudo wget -O /etc/apt/sources.list.d/ambari.list http://public-repo-1.hortonworks.com/ambari/ubuntu18/2.x/updates/2.7.3.0/ambari.list
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
-sudo apt-get update
+wget -O /etc/apt/sources.list.d/ambari.list http://public-repo-1.hortonworks.com/ambari/ubuntu18/2.x/updates/2.7.3.0/ambari.list
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+apt-get update
 
-sudo mkdir /etc/docker
+mkdir /etc/docker
 printf "{\n\t\"live-restore\": false,\n\t\"debug": true\n}" > /etc/docker/daemon.json
